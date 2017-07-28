428e6925aa9f09929b4ea7ac26401f9d08dda708
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2017-07-28 11:51:58 +0545

    routes mantained

--- src/Routes.android.js
+++ src/Routes.android.js
@@ -1,8 +1,17 @@
 import { StackNavigator } from 'react-navigation'
 import WelcomeScreen from './screens/WelcomeScreen';
+import AuthScreen from './screens/AuthScreen';
 
 const AppRouter = StackNavigator({
-  welcomeScreen: { screen: WelcomeScreen}
-})
+  WelcomeScreen: { screen: WelcomeScreen },
+  AuthScreen: {
+    screen: AuthScreen
+  }
+},
+{
+  headerMode: 'none'    
+}  
+
+)
 
 export default AppRouter;
