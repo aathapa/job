01cc37e5555982cf91893ed9637a45d00887546e
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2017-10-05 14:00:20 +0545

    action creater to fetch jobs

--- actions/jobs.actions.js
+++ actions/jobs.actions.js
@@ -1,23 +1,44 @@
-import { FETCH_JOBS } from '../constant';
+import { FETCH_JOBS, LIKE_JOB, CLEAR_JOB } from '../constant';
 import reverseGeocode from 'latlng-to-zip';
 import qs from 'qs';
+import axios from 'axios';
 
-const JOBS_QUERY_PARAMS = {
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
+const JOB_QUERY_PARAMS = {
   publisher: '4201738803816157',
-  v: '2',
   format: 'json',
+  v: '2',
   latlong: 1,
   radius: 10,
   q: 'javascript'
+};
+
+const buildJobsUrl = (zip) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
+export const fetchJobs = (region, callback) => async (dispatch) => {
+  try {
+    let zip = await reverseGeocode(region);
+    const url = buildJobsUrl(zip);
+    let { data } = await axios.get(url);
+    dispatch({ type: FETCH_JOBS, payload: data });
+    callback();
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const likedJobs = (job) => {
+  return {
+    type: LIKE_JOB,
+    payload: job 
+  }
 }
 
-export const fetchJobs = (region) => {
-  return async (dispatch) => {
-    try {
-      let zipcode = await reverseGeocode(region)
-    }
-    catch(e) {
-
-    }
+export const clearJob = () => {
+  return {
+    type: CLEAR_JOB
   }
 }
