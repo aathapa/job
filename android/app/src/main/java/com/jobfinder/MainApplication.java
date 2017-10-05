70eacb29fa7f01271d01dd0e9dd322f32cdf5659
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2017-10-05 13:48:31 +0545

    vector icon page setup in android

--- android/app/src/main/java/com/jobfinder/MainApplication.java
+++ android/app/src/main/java/com/jobfinder/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.appevents.AppEventsLogger;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new MapsPackage(),
           new FBSDKPackage(mCallbackManager)
       );
